c0e7437a47697f574ca40022cfe261ba5516bb41
Hector R Dominguez <136000082+hdominguezyork@users.noreply.github.com>
2023-10-13 14:18:35 +0000

    member.j

---
+++ member.js
@@ -0,0 +1,15 @@
+function skillsMember()
+{
+        return {
+            restrict: 'E',
+            templateUrl: 'modules/skills/views/member.html',
+            controller: 'SkillMemberController',
+            controllerAs: 'vm',
+            bindToController: true,
+            scope:{
+                member: '-'
+            }
+            
+        }
+    
+}
